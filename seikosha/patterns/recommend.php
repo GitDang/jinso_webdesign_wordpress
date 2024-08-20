9b558e8cbed118df5e7bc3d52f303cfdb48560a1
dangtm1 <minhdangit95@gmail.com>
2024-08-20 09:56:55 +0900

    commit source base

---
+++ seikosha/patterns/recommend.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * Title: Menu home
+ * Slug: themeslug/recommend
+ * Categories: featured
+ *
+ * @package Base Theme
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <section id="recommend">
+        <h2>
+            <!-- wp:paragraph {"align":"center"} -->
+            <span>Product News</span>
+            <!-- /wp:paragraph -->
+            <br>
+            <!-- wp:paragraph {"align":"center"} -->
+                誠鋼社製品のお知らせ
+            <!-- /wp:paragraph -->
+        </h2>
+        <div class="swiper-container swiper2 swiper-initialized swiper-horizontal">
+              <ul class="swiper-wrapper" id="swiper-wrapper-c106cca38c18f173f" aria-live="off" style="transition-duration: 0ms; transform: translate3d(-3896px, 0px, 0px); transition-delay: 0ms;">
+              <li class="swiper-slide" role="group" aria-label="10 / 15" data-swiper-slide-index="9" style="width: 284.667px; margin-right: 40px;"> <a href="https://rafos.jp/" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews12_01.jpg' ) ); ?>" alt="RAFOS特設サイト　RAFOSの全シリーズを詳しくご紹介"></div>
+                  <div class="text">
+                    <div class="title">RAFOSシリーズ特設サイト<br class="pc">
+                      RAFOSの全シリーズを詳しくご紹介！</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>RAFOS</li>
+                      <li>ラジオ波</li>
+                      <li>ハイパワー高周波</li>
+                      <li>筋膜</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="11 / 15" data-swiper-slide-index="10" style="width: 284.667px; margin-right: 40px;"> <a href="lp/RafosTera/" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews8.jpg' ) ); ?>" alt="筋膜、コンントゥアリング、遠隔手術のようなダヴィンチ高周波新登場！ RAFOS TERA"></div>
+                  <div class="text">
+                    <div class="title">遠隔手術のようなダヴィンチ高周波<br class="pc">
+                      RAFOS TERA新登場！</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>温熱機器</li>
+                      <li>ラジオ波</li>
+                      <li>施術機器</li>
+                      <li>筋膜</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="12 / 15" data-swiper-slide-index="11" style="width: 284.667px; margin-right: 40px;"> <a href="products/plamingo/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews9.jpg' ) ); ?>" alt="プラズマスプレーPLAMINGO"></div>
+                  <div class="text">
+                    <div class="title">プラズマ放射で肌に隙間を作り<br class="pc">
+                      TDDSで導入！ PLAMINGO</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>プラズマ治療</li>
+                      <li>ニキビ</li>
+                      <li>AGA</li>
+                      <li>施術機器</li>
+                      <li>エステ</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="13 / 15" data-swiper-slide-index="12" style="width: 284.667px; margin-right: 40px;"> <a href="products/famubo-d/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews1.jpg' ) ); ?>" alt="超音波画像測定器（皮下脂肪厚 筋厚）FAMUBO"></div>
+                  <div class="text">
+                    <div class="title">ドップラー機能が付いた<br class="pc">
+                      FAMUBO-D登場！</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>測定器</li>
+                      <li>超音波画像</li>
+                      <li>フィジオ</li>
+                      <li>カラー画像</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="14 / 15" data-swiper-slide-index="13" style="width: 284.667px; margin-right: 40px;"> <a href="products/LymphaTouch/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews2.jpg' ) ); ?>"></div>
+                  <div class="text">
+                    <div class="title">リンパの血液循環をサポート！<br class="pc">
+                      関節ケア・スポーツケアに</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>施術器</li>
+                      <li>フィジオ</li>
+                      <li>リンパ循環</li>
+                      <li>筋膜リリース</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="15 / 15" data-swiper-slide-index="14" style="width: 284.667px; margin-right: 40px;"> <a href="products/rafos_physio/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews3.jpg' ) ); ?>" alt="フィジオに特化したスポーツラフォス！筋膜リリース RAFOS　RAFOS PHYSIO"></div>
+                  <div class="text">
+                    <div class="title">フィジオに特化したRAFOS登場！<br>
+                      削る！ 剥がす！ 筋膜リリース！</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>施術機器</li>
+                      <li>スポーツフィジオ</li>
+                      <li>ラジオ波</li>
+                      <li>温熱マッサージ</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="1 / 15" data-swiper-slide-index="0" style="width: 284.667px; margin-right: 40px;"> <a href="https://www.esthe.news/" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews_estheticwired.jpg' ) ); ?>" alt=""></div>
+                  <div class="text">
+                    <div class="title">エステティック通信2月号　株式会社誠鋼社　松村秀一（CEO）堀家潤子 (常務取締役)が掲載！ <br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">掲載情報</li>
+                      <li>&gt;エステティック通信2月号</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="2 / 15" data-swiper-slide-index="1" style="width: 284.667px; margin-right: 40px;"> <a href="https://www.seikosha-net.co.jp/products/esthelux/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews_melatite.jpg' ) ); ?>" alt=""></div>
+                  <div class="text">
+                    <div class="title">あなたのサロンに革命をもたらす フォトバイオセラピー！<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>エステラックス</li>
+                      <li>メラタイト</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="3 / 15" data-swiper-slide-index="2" style="width: 284.667px; margin-right: 40px;"> <a href="#3rafos">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews_rafos_new3.jpg' ) ); ?>" alt=""></div>
+                  <div class="text">
+                    <div class="title">RAFOS シリーズ3機種登場！！<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>&gt;RAFOS シリーズ</li>
+                      <li>ニュースリリース</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="4 / 15" data-swiper-slide-index="3" style="width: 284.667px; margin-right: 40px;"> <a href="pdf/products/famubo_brothers.pdf" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews10.jpg' ) ); ?>" alt="FAMUBO 5brothers 超音波画像測定器"></div>
+                  <div class="text">
+                    <div class="title">超音波画像測定器FAMUBOシリーズ<br class="pc">
+                      5兄弟を比べてわかるPDF</div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>測定機器</li>
+                      <li>超音波画像</li>
+                      <li>ドップラー</li>
+                      <li>Wi-Fi</li>
+                      <li>コンパクト</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="5 / 15" data-swiper-slide-index="4" style="width: 284.667px; margin-right: 40px;"> <a href="https://www.seikosha-net.co.jp/catalog/best_item/?pNo=1" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/products_best_item.jpg' ) ); ?>" alt="エステティック通信主催　ESTHETIC WIRED　BEST ITEM　アワード　2022にRAFOS Premiumがノミネートしました！"></div>
+                  <div class="text">
+                    <div class="title">エステティック通信主催　ESTHETIC WIRED　BEST ITEM　アワード　2022にRAFOS Premiumがノミネートしました！<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>RAFOS</li>
+                      <li>ラフォス</li>
+                      <li>ニュースリリース</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide swiper-slide-prev" role="group" aria-label="6 / 15" data-swiper-slide-index="5" style="width: 284.667px; margin-right: 40px;"> <a href="/products/rafos_physio/#mongol" target="_blank">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/products_news_mongol.png' ) ); ?>" alt=""></div>
+                  <div class="text">
+                    <div class="title">RAFOSがモンゴル柔道選手の減量にも大活躍！銅メダル2枚獲得おめでとうございます！<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>RAFOS</li>
+                      <li>ラフォス</li>
+                      <li>ニュースリリース</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide swiper-slide-active" role="group" aria-label="7 / 15" data-swiper-slide-index="6" style="width: 284.667px; margin-right: 40px;"> <a href="/products/starvac/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews_starvac.jpg' ) ); ?>" alt=""></div>
+                  <div class="text">
+                    <div class="title">フランス、スターバックが 新たな機種を揃えて日本参上!!<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>STARVAC</li>
+                      <li>DX twint</li>
+                      <li>DX smart</li>
+                      <li>DX light</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide swiper-slide-next" role="group" aria-label="8 / 15" data-swiper-slide-index="7" style="width: 284.667px; margin-right: 40px;"> <a href="/lp_sin/01/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews14_01.jpg' ) ); ?>" alt="RAFOS シリーズ最高傑作RAFOS TERA"></div>
+                  <div class="text">
+                    <div class="title">RAFOS シリーズ最高傑作RAFOS TERA<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>RAFOS TERA</li>
+                      <li>RAFOS シリーズ最高傑作</li>
+                    </ul>
+                  </div>
+                  </a> </li><li class="swiper-slide" role="group" aria-label="9 / 15" data-swiper-slide-index="8" style="width: 284.667px; margin-right: 40px;"> <a href="/products/famubo/">
+                  <div class="thumb"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/top_products/pronews13_01.jpg' ) ); ?>" alt="FAMUBO 医療機器認証番号取得"></div>
+                  <div class="text">
+                    <div class="title">FAMUBO 医療機器認証番号取得<br class="pc">
+                    </div>
+                    <ul>
+                      <li class="aka_waku">新商品</li>
+                      <li>FAMUBO</li>
+                      <li>医療機器認証番号</li>
+                    </ul>
+                  </div>
+                  </a> </li></ul>
+              <div class="swiper-pagination swiper-pagination-black swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" aria-current="true"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+            <div class="swiper-button-prev prev1" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-c106cca38c18f173f"></div>
+            <div class="swiper-button-next next1" tabindex="0" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-c106cca38c18f173f"></div>
+    </section>
+</div>
+<!-- /wp:group -->
