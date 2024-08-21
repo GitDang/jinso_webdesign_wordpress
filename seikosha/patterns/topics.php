ab808330850f15f5d88e59e7f65671da3ecd75a9
dangtm1 <minhdangit95@gmail.com>
2024-08-21 15:14:40 +0900

    commit block topic 2 - finished

--- seikosha/patterns/topics.php
+++ seikosha/patterns/topics.php
@@ -11,33 +11,84 @@
  */
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-  <section id="topics">
-    <div class="over_inner">
-      <h2><span>TOPICS</span><br>トピックス</h2>
-      <!--loopSliderを止めてフレックス表示-->
-      <div class="loopSlider">
-        <li class="topics__item"> <a href="https://seikosha-webshop.com/" target="_blank"> <img class="topics-list-image" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/topic/bnr_03.jpg' ) ); ?>"/>
-            <p class="topics__txt">プロ専用商材ショップ</p>
-            <p class="topics__txt_sub">登録ユーザー向け</p>
-          </a> </li>
-        <li class="topics__item"> <a href="optimus/BodyContouring.html"> <img class="topics-list-image" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/topic/bnr_04.jpg' ) ); ?>"/>
-            <p class="topics__txt">癒されながら学ぶ</p>
-            <p class="topics__txt_sub">施術する側される側</p>
-          </a> </li>
-        <li class="topics__item"> <a href="self_medication/"> <img class="topics-list-image" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/topic/bnr_05.jpg' ) ); ?>"/>
-            <p class="topics__txt">SELF MEDICATION</p>
-            <p class="topics__txt_sub">健康習慣を身につけるお手伝い</p>
-          </a> </li>
-        <li class="topics__item"> <a href="company/evidence.html"> <img class="topics-list-image" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/topic/bnr_01.jpg' ) ); ?>"/>
-            <p class="topics__txt">EVIDENCE</p>
-            <p class="topics__txt_sub">健康と美を追求する</p>
-          </a> </li>
-        <li class="topics__item"> <a href="https://www.rakuten.co.jp/seikosha/" target="_blank"> <img class="topics-list-image" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/topic/bnr_02.jpg' ) ); ?>"/>
-            <p class="topics__txt">公式オンラインショップ</p>
-            <p class="topics__txt_sub">個人のお客様向け</p>
-          </a> </li>
-      </div>
+<!-- wp:group -->
+<div class="wp-block-group block-topics">
+  <!-- wp:paragraph {"align":"center"} -->
+  <h2><span>TOPICS</span><br>トピックス</h2><!-- /wp:paragraph -->
+  <!-- wp:group -->
+  <div class="wp-block-group list-topics">
+
+    <!-- wp:group -->
+    <div class="wp-block-group item-topics">
+      <!-- wp:image -->
+      <figure class="wp-block-image">
+        <a href="https://seikosha-webshop.com/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/topic/bnr_03.jpg')); ?>" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt">プロ専用商材ショップ</p><!-- /wp:paragraph -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt-sub">登録ユーザー向け</p><!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group item-topics">
+      <!-- wp:image -->
+      <figure class="wp-block-image">
+        <a href="optimus/BodyContouring.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/topic/bnr_04.jpg')); ?>" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt">癒されながら学ぶ</p><!-- /wp:paragraph -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt-sub">施術する側される側</p><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group item-topics">
+      <!-- wp:image -->
+      <figure class="wp-block-image">
+        <a href="self_medication/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/topic/bnr_05.jpg')); ?>" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt">SELF MEDICATION</p><!-- /wp:paragraph -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt-sub">健康習慣を身につけるお手伝い</p><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group item-topics">
+      <!-- wp:image -->
+      <figure class="wp-block-image">
+        <a href="company/evidence.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/topic/bnr_01.jpg')); ?>" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt">EVIDENCE</p><!-- /wp:paragraph -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt-sub">健康と美を追求する</p><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group item-topics">
+      <!-- wp:image -->
+      <figure class="wp-block-image">
+        <a href="https://www.rakuten.co.jp/seikosha/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/topic/bnr_02.jpg')); ?>" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt">公式オンラインショップ</p><!-- /wp:paragraph -->
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="topics-txt-sub">個人のお客様向け</p><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
