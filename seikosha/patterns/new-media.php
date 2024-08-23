743dad2017f13bfe632a950cd7e06084f1ea96ae
dangtm1 <minhdangit95@gmail.com>
2024-08-23 19:07:45 +0900

    commit block media

---
+++ seikosha/patterns/new-media.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * Title: New Media
+ * 
+ * Slug: themeslug/new-media
+ * Categories: featured
+ *
+ * @package Base Theme
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group block-new-media">
+  <!-- wp:paragraph {"align":"center"} -->
+  <h2><span>News & Media</span><br>お知らせ</h2><!-- /wp:paragraph -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group list-new-media">
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <a href="NewsMedia/demonstration.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/mv_thumbnail1.jpg')); ?>" /></a>
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <a href="NewsMedia/demonstration.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/mv_thumbnail2.jpg')); ?>" /></a>
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <a href="NewsMedia/demonstration.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/mv_thumbnail3.jpg')); ?>" /></a>
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group list-new-media-info">
+
+    <!-- wp:group -->
+    <div class="wp-block-group new-media-info-item">
+      <!-- wp:image  -->
+      <figure class="wp-block-image">
+        <a href="NewsMedia/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/news_01.jpg')); ?>" alt="誠鋼社のお知らせ INFORMATION" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} --><span class="new-media-info-image-title">お知らせ</span><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group new-media-info-item">
+      <!-- wp:image  -->
+      <figure class="wp-block-image">
+        <a href="NewsMedia/Interview.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/news_02.jpg')); ?>" alt="誠鋼社のお知らせ INFORMATION" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} --><span class="new-media-info-image-title">インタビュー</span><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group new-media-info-item">
+      <!-- wp:image  -->
+      <figure class="wp-block-image">
+        <a href="NewsMedia/media.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/news_03.jpg')); ?>" alt="誠鋼社のお知らせ INFORMATION" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} --><span class="new-media-info-image-title">メディア掲載情報</span><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group new-media-info-item">
+      <!-- wp:image  -->
+      <figure class="wp-block-image">
+        <a href="NewsMedia/faq.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/news_04.jpg')); ?>" alt="誠鋼社のお知らせ INFORMATION" /></a>
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph {"align":"center"} --><span class="new-media-info-image-title">よくあるご質問</span><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group new-media-list-box">
+    <!-- wp:group -->
+    <div class="wp-block-group new-media-box-header">
+      <!-- wp:paragraph --><span class="new-media-box-title">誠鋼社のお知らせ</span><!-- /wp:paragraph -->
+      <!-- wp:buttons -->
+      <div class="wp-block-buttons">
+        <!-- wp:button -->
+        <div class="wp-block-button">
+          <a class="wp-block-button__link" href="NewsMedia/index.html" target="_blank">more</a>
+        </div>
+        <!-- /wp:button -->
+      </div>
+      <!-- /wp:buttons -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"20%"} -->
+      <div class="wp-block-column new-media-box-date" style="flex-basis:20%">
+        <!-- wp:paragraph --><span>2024/7/26</span><!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:column -->
+      <!-- wp:column {"width":"77%"} -->
+      <div class="wp-block-column new-media-box-content" style="flex-basis:77%">
+        <!-- wp:paragraph --><span>【誠鋼社2024年度夏季休業のお知らせ】2024年8月10日(土)～8月15日(木)夏季休業となります。期間中とその前後、配送に遅れが生じる場合がございます。何卒ご了承くださいませ。</span><!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+     <!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"20%"} -->
+      <div class="wp-block-column new-media-box-date" style="flex-basis:20%">
+        <!-- wp:paragraph --><span>2024/5/21</span><!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:column -->
+      <!-- wp:column {"width":"77%"} -->
+      <div class="wp-block-column new-media-box-content" style="flex-basis:77%">
+        <!-- wp:paragraph --><span>ビューティーワールドジャパン東京2024展示会出展の様子を掲載いたしました！詳細ページはこちら</span><!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group new-media-btn">
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <a href="NewsMedia/demonstration.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/btn_mv1.jpg')); ?>" /></a>
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <a href="company/evidence.html" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/new_media/btn_mv2.jpg')); ?>" /></a>
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
