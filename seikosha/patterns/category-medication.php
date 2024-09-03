1efeb364c15f3de8363872b172c6f57c9446e75f
dangtm1 <minhdangit95@gmail.com>
2024-09-03 15:18:22 +0900

    commit block category child selfmedication

---
+++ seikosha/patterns/category-medication.php
@@ -0,0 +1,369 @@
+<?php
+
+/**
+ * Title: Category Medication Page
+ * 
+ * Slug: themeslug/category-medication
+ * Categories: font-page
+ *
+ * @package Base Theme
+ * @since 1.0.0
+ */
+?>
+
+
+
+<!-- wp:group -->
+<div class="wp-block-group category-medication-banner">
+    <!-- wp:image -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/bg_selfmedication.png')); ?>" /></figure>
+    <!-- /wp:image -->
+    <!-- wp:group -->
+    <div class="wp-block-group sub-image">
+        <!-- wp:image -->
+        <figure class="wp-block-image"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/tit_header.png')); ?>" /></figure>
+        <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group category-medical-title">
+    <!-- wp:group -->
+    <div class="wp-block-group category-medical-list">
+        <!-- wp:paragraph -->
+        <p class="category-medical-link category-title-header"><a href="/"></a></p><!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="category-medical-link category-title-header"><a href="index.html">製品一覧</a></p><!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="category-title-header">Self Medication</p><!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons list-button-social">
+    <!-- wp:button -->
+    <div class="wp-block-button"><a class="wp-block-button__link" href="https://www.instagram.com/seikosha.inc/" target="_blank"><img class="button-link-social" width="24" src="<?php echo esc_url(get_theme_file_uri('assets/images/common/icon_instagram.png')); ?>" alt="誠鋼社のInstagram">誠鋼社公式Instagram
+        </a></div>
+    <!-- /wp:button -->
+    <!-- wp:button -->
+    <div class="wp-block-button"><a class="wp-block-button__link" href="https://www.instagram.com/optimusshop_official/" target="_blank"><img class="button-link-social" width="24" src="<?php echo esc_url(get_theme_file_uri('assets/images/common/icon_instagram.png')); ?>" alt="誠鋼社のInstagram">OPTIMUS SHOP</a></div>
+    <!-- /wp:button -->
+</div>
+<!-- /wp:buttons -->
+
+<!-- wp:group -->
+<div class="wp-block-group category-medication">
+    <!-- area 1 -->
+    <!-- wp:group -->
+    <div class="wp-block-group category-medication-title">
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-header">Home esthetic</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-sub">ホームエステ</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group category-medication-product">
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div v class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="../products/R_face/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_11_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">R face　ラフォスフェイス</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">小型の高周波温熱機器だから、ご自宅用やプチサロン、美容室、ネイルサロンでもどこでもかんたんリフトアップ</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rfc-01/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products11.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">Pure Face RF Cream</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">肌表面にやさしく密着するR.face専用のRFクリームです。アロマオイルの香りに包まれ、お肌をうるおいケアします。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rm-101/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_09_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">RAFOS mini　ラフォスミニ</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">持運び可能な高周波温熱機器。ご自分（セルフ）で簡単に施術することが可能なので、ご自宅用にもたいへん人気があります。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- area 2 -->
+    <!-- wp:group -->
+    <div class="wp-block-group category-medication-title">
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-header">Facial / Body Care</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-sub">フェイシャル / ボディケア</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- wp:group -->
+    <div class="wp-block-group category-medication-product">
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div v class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="../products/R_face/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/body.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">PLINIC</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">自宅でプラズマスキンケアとリフティングができます。殺菌力でニキビやアトピーに。浸透力で美容液や導入薬剤を素早く浸透させ、くすみのない肌に。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rfc-01/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products14.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">PLシートマスク</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">肌の乾燥、しみ、くすみにお悩みの方や化粧ノリが気になる方へ。ミネラル成分やアミノ酸を含有した真珠エキスで、弾力あるしっとりした肌に。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rm-101/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_21_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">PPCオイル</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">大豆から抽出されたホスファチジルコリンを5000PPMの高濃度で配合!　あなたに黄金バランスを</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- area 3 -->
+    <!-- wp:group -->
+    <div class="wp-block-group category-medication-title">
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-header">Canal Series</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="category-medication-title-sub">カナルシリーズ</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+    <div class="wp-block-group category-medication-product">
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div v class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="../products/R_face/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_25_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">Canal凹凸ベルト</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">連続した凹凸のほどよい弾力性と優れた通気性。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rfc-01/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_26_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">Canal凹凸パッド</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">カナル(運河)療法の理論から生まれたカラダに優しい循環路確保パッド。</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rm-101/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_27_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">Canal凹凸ピロー</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">医療現場から生まれた凹凸形状で心地よい眠りを。!　あなたに黄金バランスを</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group category-medication-product-item">
+            <!-- wp:group -->
+            <div class="wp-block-group category-medication-product-item-up">
+                <!-- wp:image -->
+                <figure class="wp-block-image">
+                    <a href="https://item.rakuten.co.jp/seikosha/rm-101/" target="_blank"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/category/products_29_l.jpg')); ?>" /></a>
+                </figure>
+                <!-- /wp:image -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt">Canalソフトシーネ</p><!-- /wp:paragraph -->
+                <!-- wp:paragraph {"align":"center"} -->
+                <p class="product-item-txt-sub">カナル療法の理論から生まれた包帯・テーピング固定下の新素材。!　あなたに黄金バランスを</p><!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link" href="form/contact/mailform/" target="_blank">ご購入はこちら</a>
+            </div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group btn-area">
+        <!-- /wp:group --> <!-- wp:group -->
+        <div class="wp-block-group category-medication-image">
+            <!-- wp:image {"id":66,"sizeSlug":"large","linkDestination":"none"} -->
+            <figure class="wp-block-image size-large">
+                <a class="product-item-link" href="https://www.rakuten.co.jp/seikosha/" target="_blank">
+                    <img src="<?php echo esc_url(get_theme_file_uri('assets/images/common/bnr_rakuten.png')); ?>" alt="OPTIMUS 楽天オンライン" class="wp-image-66" /></a>
+            </figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:group --> <!-- wp:group -->
+        <div class="wp-block-group category-medication-image">
+            <!-- wp:image {"id":66,"sizeSlug":"large","linkDestination":"none"} -->
+            <figure class="wp-block-image size-large">
+                <a class="product-item-link" href="https://seikosha-webshop.com/" target="_blank">
+                    <img src="<?php echo esc_url(get_theme_file_uri('assets/images/common/bnr_btob.png')); ?>" alt="誠鋼社プロ専用商材ショップ" class="wp-image-66" /></a>
+            </figure>
+            <!-- /wp:image -->
+        </div>
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group category-page-division">
+        <!-- wp:group -->
+        <div class="wp-block-group category-division-list">
+            <!-- wp:paragraph -->
+            <p class="category-division-link link-medical"><a href="medical.html"><span class="category-division-link-text">Medical</span></a></p><!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="category-division-link link-beauty"><a href="beauty.html"><span class="category-division-link-text">Beauty</span></a></p><!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="category-division-link link-physio"><a href="physio.html" class="category-division-link-text"><span>Physio</span></a></p><!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+     
+</div>
+<!-- /wp:group -->
